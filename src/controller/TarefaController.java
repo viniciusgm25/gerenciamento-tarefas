2c691d6c2373d34a0a7d09d10b670d7326d6a268
VINICIUS GONCALVES MESQUITA <vinicius.goncalves2503@gmail.com>
2026-02-08 04:16:06 -0300

    Create TarefaController.java

---
+++ src/controller/TarefaController.java
@@ -0,0 +1,36 @@
+package com.vinicius.tarefas.controller;
+
+import com.vinicius.tarefas.dto.TarefaRequestDTO;
+import com.vinicius.tarefas.dto.TarefaResponseDTO;
+import com.vinicius.tarefas.service.TarefaService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tarefas")
+public class TarefaController {
+
+    private final TarefaService service;
+
+    public TarefaController(TarefaService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<TarefaResponseDTO> listar() {
+        return service.listar();
+    }
+
+    @PostMapping
+    public TarefaResponseDTO criar(
+            @RequestBody @Valid TarefaRequestDTO dto) {
+        return service.criar(dto);
+    }
+
+    @PutMapping("/{id}/concluir")
+    public void concluir(@PathVariable Long id) {
+        service.concluir(id);
+    }
+}
