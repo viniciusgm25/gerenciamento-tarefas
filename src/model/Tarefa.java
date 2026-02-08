d842aba1547bc881d3adca1398825da261ac751b
VINICIUS GONCALVES MESQUITA <vinicius.goncalves2503@gmail.com>
2026-02-08 04:13:10 -0300

    Create Tarefa.java

---
+++ src/model/Tarefa.java
@@ -0,0 +1,52 @@
+package com.vinicius.tarefas.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Tarefa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+    private String descricao;
+    private boolean concluida;
+
+    public Tarefa() {
+    }
+
+    public Tarefa(String titulo, String descricao) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.concluida = false;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public boolean isConcluida() {
+        return concluida;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setConcluida(boolean concluida) {
+        this.concluida = concluida;
+    }
+}
